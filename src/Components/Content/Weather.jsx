c806c4ad4fc6dcded9d71ae8bedef56b902b7828
Nghia Nguyen <ainzynguyen95@gmail.com>
2024-03-30 00:11:23 -0700

    commit

---
+++ src/Components/Content/Weather.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function Weather() {
+    const [data, setData] = useState("")
+    const fetchData = async () => {
+        // const respone = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+        const respone = await fetch(`https://api.weatherapi.com/v1/current.json?key=6b23328d66f744eaa6934533241503&q=Paris`)
+
+        const jsonData = await respone.json()
+        setData(jsonData)
+    }
+
+    return <>
+        <h1>Fetching data</h1>
+        <button className="button" onClick={fetchData}>Fetch data</button>
+        {data && <p>{JSON.stringify(data)}</p>}
+    </>
+}
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,6 @@
+export default function NavBar() {
+    return <div>
+        <nav>Weather App</nav>
+        <img src="https://static.vecteezy.com/system/resources/previews/012/066/505/original/sunny-and-rainy-day-weather-forecast-icon-meteorological-sign-3d-render-png.png" alt="" width={150} />
+    </div>
+}
